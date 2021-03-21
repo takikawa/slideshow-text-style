c94e6c7f9928777df0e12205b5aa76b59d93ee1c
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2021-03-21 13:55:07 +0700

    Make pkg-build ignore example.rkt
    pkg-build attempts to test example.rkt, but this will always timeout
    because slideshow is waiting for an input from users. One possible
    solution is to add the file to test-omit-paths when pkg-build is
    detected.

--- info.rkt
+++ info.rkt
@@ -4,3 +4,4 @@
 (define build-deps '("scribble-lib" "at-exp-lib" "pict-doc"
                      "slideshow-doc" "racket-doc" "scribble-doc"))
 (define scribblings '(("slideshow-text-style.scrbl" () ("Slideshow Libraries"))))
+(define test-omit-paths (if (getenv "PLT_PKG_BUILD_SERVICE") '("example.rkt") '()))
